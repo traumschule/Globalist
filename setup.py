ba1d70af2c1f1c1b814806c84f44f35151136b12
fnordomat <46D46D1246803312401472B5A7427E237B7908CA>
2017-02-25 17:25:17 +0000

    installscript and rudimentary module structure

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+    name='Globalist',
+    version='0.0.3',
+    description='Globalist distributed git onions',
+    author='fnordomat',
+#    author_email='',
+    url='https://github.com/fnordomat/Globalist',
+    packages=['globalist'],
+    license='GPLv3'
+)
+
